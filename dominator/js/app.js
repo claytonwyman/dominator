fa97873d39a3b5831d8721afdac3c4321fe8f377
silver54clay <32491636+sivler54clay@users.noreply.github.com>
2019-05-02 10:42:07 -0600

    2nd Commit
    Added new functionality

--- dominator/js/app.js
+++ dominator/js/app.js
@@ -1,8 +1,17 @@
 const inputs = document.querySelectorAll('input');
+const page = document.getElementById('page');
 
 
 function create_Ele(){
     let element = document.createElement(inputs[0].value);
+    for(let i=1; i < inputs.length; i++) {
+        let attribute = inputs[i].getAttribute('placeholder');
+        let value = inputs[i].value;
+        if (value) {
+            element[attribute] = value;
+        }
+        page.appendChild(element);
+    }
     console.log(element);
 
 }
